d137d63c604ab04c79a793b8940f7903c5284cb7
Muriel Salvan <Muriel@X-Aeon.com>
2012-07-23 15:59:53 +0200

    Added a nice Rails application example using rails-ajax.

---
+++ examples/my_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRailsApp::Application.config.secret_token = '9eb53f6a7f8871be674b8715da8f54a8302963e973eac62bced3ab659ca67a20e4cbffae90f9509cc64bd881dfa3b40435f84aa33db5d51cafd2d301006d8868'
