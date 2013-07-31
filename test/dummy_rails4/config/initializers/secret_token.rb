bf37794c5a9bd62910677aac151b3d5f95ef6347
Muriel Salvan <Muriel@X-Aeon.com>
2013-07-31 18:44:36 +0200

    Integrate same test cases running for both Rails 3 and 4

---
+++ test/dummy_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '5471ac91101cb8cd7616c5a13ab04dbe310a51f5ce83074f608fa95071317608cb51af2f3e0e64943d036297cdfa2112076a8711caa5c38de89748291cb10d90'
--- test/run.rb
+++ test/run.rb
@@ -3,7 +3,29 @@
 # Licensed under the terms specified in LICENSE file. No warranty is provided.
 #++
 
-Dir.chdir("#{File.dirname(__FILE__)}/dummy")
-system('cucumber')
-exit_status = $?.exitstatus
-raise "cucumber failed with exit status #{exit_status}" unless (exit_status == 0)
+test_root_dir = File.expand_path(File.dirname(__FILE__))
+errors = []
+[
+  'dummy_rails3',
+  'dummy_rails4'
+].each do |test_directory|
+	Bundler.with_clean_env do
+		puts
+		puts
+		puts "======= Executing test suite in #{test_directory} ======="
+		rails_root_dir = "#{test_root_dir}/#{test_directory}"
+		Dir.chdir(rails_root_dir)
+		# cucumber/rails uses RAILS_ROOT to know where the Rails application is.
+		# Force it as otherwise it looks in features/../..
+		ENV['RAILS_ROOT'] = rails_root_dir
+		# Make sure we have correct Gem versions
+		system('bundle install')
+		system('bundle exec cucumber ../features')
+		exit_status = $?.exitstatus
+		errors << "[#{test_directory}] - cucumber failed with exit status #{exit_status}" if (exit_status != 0)
+		puts
+		puts
+	end
+end
+
+raise "#{errors.size} failed cucumber test suites:\n#{errors.join("\n")}" if (!errors.empty?)
